255061597ea3855f3e03730dd094985cb011b85c
linds0202 <lindsayarainey@gmail.com>
2023-02-23 09:27:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Install
+
+`npm install`
+
+---
+
+# Things to add
+
+- Create a `.env` file in config folder and add the following as `key = value`
+  - PORT = 2121 (can be any port example: 3000)
+  - DB_STRING = `your database URI`
+  - CLOUD_NAME = `your cloudinary cloud name`
+  - API_KEY = `your cloudinary api key`
+  - API_SECRET = `your cloudinary api secret`
+
+---
+
+# Run
+
+`npm start`
+
+
+<!-- 
+  
+  * all icons provided by <a href="https://www.flaticon.com/free-icons/spiral" title="spiral icons">Spiral icons created by Freepik - Flaticon</a> * 
+  
+  * hero photo credit: Photo by Ivica Džambo: https://www.pexels.com/photo/a-curvy-road-surrounded-by-trees-6547114/ or 
+  Photo by eberhard grossgasteiger: https://www.pexels.com/photo/aerial-photography-of-zig-zag-road-1612462/ *
+
+
+-->
---
+++ controllers/task.js
@@ -0,0 +1,59 @@
+const cloudinary = require("../middleware/cloudinary")
+const Task = require("../models/Task")
+
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+      const tasks = await Task.find({ user: req.user.id });
+      res.render("profile.ejs", { tasks: tasks, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }}
+//   ,
+//   getFeed: async (req, res) => {
+//     try {
+//       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+//       res.render("feed.ejs", { posts: posts });
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+//   getPost: async (req, res) => {
+//     try {
+//       const post = await Post.findById(req.params.id);
+//       const comments = await Comment.find({ post: req.params.id }).sort({ createdAt: "desc" }).lean().populate('user');
+//       console.log(comments)
+//       res.render("post.ejs", { post: post, user: req.user, comments: comments });
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+//   createPost: async (req, res) => {
+//     try {
+
+//       await Post.create({
+//         item: req.body.item,
+//         sale: req.body.sale,
+//         price: req.body.price,
+//         user: req.user.id,
+//       });
+//       console.log("Item has been added!");
+//       res.redirect("/profile");
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+//   deletePost: async (req, res) => {
+//     try {
+//       // Find post by id
+//       let post = await Post.findById({ _id: req.params.id });
+//       // Delete post from db
+//       await Post.remove({ _id: req.params.id });
+//       console.log("Deleted Item");
+//       res.redirect("/profile");
+//     } catch (err) {
+//       res.redirect("/profile");
+//     }
+//   },
+}
